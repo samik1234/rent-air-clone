da43634b24649e787f85122d35b9f507d9ddad81
samiksha <samikshawankhade11@gmail.com>
2023-09-20 15:29:16 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
+      appDir: true,
     },
     images: {
-        domains: [
-            "res.cloudinary.com", 
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com"
-        ]
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
     }
-
 }
-
+  
 module.exports = nextConfig
